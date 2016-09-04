42793115af942956d6a49c777f9cf977add4cb38
atonkoshkurov <moscowroller@gmail.com>
2016-09-04 12:10:39 +0300

    a first part of classess

---
+++ classes/ActionTypes.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* ActionTypes class - Enumeration of page action types
+*
+* @version 1.0
+*/
+Abstract Class ActionTypes
+{
+	const Add = 0;
+	const Edit = 1;
+	const Remove = 2;
+}
+?>
